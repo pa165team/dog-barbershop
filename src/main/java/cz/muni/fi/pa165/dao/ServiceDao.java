d515060eb8a3de2020b18e2a96c578eaf2f22e67
Jan Kalfus <410455@mail.muni.cz>
2017-10-25 20:47:16 +0200

    #7 Created Service entity + dao + dao implementation

---
+++ src/main/java/cz/muni/fi/pa165/dao/ServiceDao.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pa165.dao;
+
+import cz.muni.fi.pa165.entity.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Jan Kalfus
+ */
+public interface ServiceDao {
+    Service findById(Long id);
+
+    void create(Service service);
+
+    void delete(Service service);
+
+    List<Service> findAll();
+
+    /**
+     * Gets services based on their description
+     *
+     * @param description Description
+     * @return All services containing the description string, or null if no match is found.
+     */
+    List<Service> getAllMatchingDescription(String description);
+
+    /**
+     * Gets services that were provided between two dates
+     *
+     * @param start First date
+     * @param end   Second date
+     * @return All matching services, or null if no services match.
+     */
+    List<Service> getServicesProvidedBetween(Date start, Date end);
+}
