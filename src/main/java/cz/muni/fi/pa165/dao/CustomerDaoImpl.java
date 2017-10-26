3013288acc3d4fb7895d789dd83c5a0475abd095
Jan Kalfus <32128382+410455@users.noreply.github.com>
2017-10-26 20:24:29 +0200

    Merge pull request #9 from lucikol/master
    Added customer entity, DAO and DAO implementation
    # Conflicts:
    #	src/main/java/cz/muni/fi/pa165/dao/CustomerDaoImpl.java

--- src/main/java/cz/muni/fi/pa165/dao/CustomerDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/dao/CustomerDaoImpl.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
-
 import org.springframework.stereotype.Repository;
 
 /**
@@ -65,4 +64,3 @@ public class CustomerDaoImpl implements CustomerDao{
         }
 
     }
-}
