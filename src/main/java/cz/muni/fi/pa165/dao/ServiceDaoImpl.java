321b590843dc165eea4a8dce6b3e494b8f2f164c
Jan Kalfus <410455@mail.muni.cz>
2017-10-29 13:21:45 +0100

    Removed unnecessary try-catch from ServiceDaoImpl

--- src/main/java/cz/muni/fi/pa165/dao/ServiceDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/dao/ServiceDaoImpl.java
@@ -4,7 +4,6 @@ import cz.muni.fi.pa165.entity.Service;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.Date;
 import java.util.List;
@@ -36,24 +35,16 @@ public class ServiceDaoImpl implements ServiceDao {
     }
 
     public List<Service> getAllMatchingDescription(String description) {
-        try {
-            return em.createQuery("SELECT s FROM Service s WHERE s.description LIKE :description", Service.class)
-                .setParameter("description", "%" + description + "%")
-                .getResultList();
-        } catch (NoResultException ex) {
-            return null;
-        }
+        return em.createQuery("SELECT s FROM Service s WHERE s.description LIKE :description", Service.class)
+            .setParameter("description", "%" + description + "%")
+            .getResultList();
     }
 
     public List<Service> getServicesProvidedBetween(Date start, Date end) {
-        try {
-            return em
-                .createQuery("SELECT s FROM Service s WHERE s.dateProvided BETWEEN :startDate AND :endDate", Service.class)
-                .setParameter("startDate", start)
-                .setParameter("endDate", end)
-                .getResultList();
-        } catch (NoResultException ex) {
-            return null;
-        }
+        return em
+            .createQuery("SELECT s FROM Service s WHERE s.dateProvided BETWEEN :startDate AND :endDate", Service.class)
+            .setParameter("startDate", start)
+            .setParameter("endDate", end)
+            .getResultList();
     }
 }
