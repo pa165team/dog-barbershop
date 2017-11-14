229c03d1e0e1650def3f98fd8dec314e557d26b0
Martin <martin.k738@gmail.com>
2017-11-14 16:50:02 +0100

    first impl of Dog facades and services

--- dog-barbershop-business/src/main/java/cz/muni/fi/pa165/facade/DogFacade.java
+++ dog-barbershop-business/src/main/java/cz/muni/fi/pa165/facade/DogFacade.java
@@ -1,8 +1,18 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.DogCreateDTO;
+import cz.muni.fi.pa165.dto.DogDTO;
+import cz.muni.fi.pa165.enums.Gender;
+
+import java.util.List;
+
 /**
  * @author Martin Kuchar 433499
  */
 
 public interface DogFacade {
+    List<DogDTO> getAllDogs();
+    DogDTO getDogById(Long id);
+    List<DogDTO> getAllDogsOfGender(Gender gender);
+    Long createDog(DogCreateDTO newDog);
 }
--- dog-barbershop-business/src/main/java/cz/muni/fi/pa165/facade/DogFacadeImpl.java
+++ dog-barbershop-business/src/main/java/cz/muni/fi/pa165/facade/DogFacadeImpl.java
@@ -1,8 +1,54 @@
 package cz.muni.fi.pa165.facade;
 
+import cz.muni.fi.pa165.dto.DogCreateDTO;
+import cz.muni.fi.pa165.dto.DogDTO;
+import cz.muni.fi.pa165.entity.Dog;
+import cz.muni.fi.pa165.enums.Gender;
+import cz.muni.fi.pa165.service.BeanMappingService;
+import cz.muni.fi.pa165.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * @author Martin Kuchar 433499
  */
 
+@Service
+@Transactional
 public class DogFacadeImpl implements DogFacade{
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    @Autowired
+    private DogService dogService;
+
+    @Override
+    public List<DogDTO> getAllDogs() {
+        return beanMappingService.mapTo(dogService.findAll(), DogDTO.class);
+    }
+
+    @Override
+    public DogDTO getDogById(Long id) {
+        return beanMappingService.mapTo(dogService.findById(id), DogDTO.class);
+    }
+
+    @Override
+    public List<DogDTO> getAllDogsOfGender(Gender gender) {
+        return beanMappingService.mapTo(dogService.findAllOfGender(gender), DogDTO.class);
+    }
+
+    @Override
+    public Long createDog(DogCreateDTO newDog) {
+        Dog dog = new Dog();
+        dog.setName(newDog.getName());
+        dog.setBreed(newDog.getBreed());
+        dog.setDateOfBirth(newDog.getDateOfBirth());
+        dog.setGender(newDog.getGender());
+        dogService.create(dog);
+        return dog.getId();
+    }
 }
