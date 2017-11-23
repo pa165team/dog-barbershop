41555b96c93607e377b9af73251f3528192b2ca6
muddan165 <32128596+muddan165@users.noreply.github.com>
2017-11-23 06:07:02 +0100

    Employee (facade+service+dto) - no service tests yet (#33)
    * Employee: facade+facadeImpl+service+serviceImpl+facadeTest (no service tests yet)
    
    * Employee (facade etc) v1.1 - fixed equals, setid, facade.update
    
    * Employee (facade etc) v1.2 - removed unnecessary update

--- dog-barbershop-service/src/main/java/cz/muni/fi/pa165/service/EmployeeService.java
+++ dog-barbershop-service/src/main/java/cz/muni/fi/pa165/service/EmployeeService.java
@@ -1,9 +1,25 @@
 package cz.muni.fi.pa165.service;
 
 import cz.muni.fi.pa165.entity.Employee;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author Daniel Mudrik (433655)
+ */
+@Service
 public interface EmployeeService {
-    // TODO: prosím dodělat
-
     Employee findById(Long id);
+
+    List<Employee> findAll();
+
+    void create(Employee employee);
+
+    void remove(Long employeeId);
+
+    List<Employee> findAllBelowSalary(BigDecimal salary);
+
+    void update(Employee employee);
 }
