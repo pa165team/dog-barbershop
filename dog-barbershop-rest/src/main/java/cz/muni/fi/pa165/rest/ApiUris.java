edbcc3c5df92620705fa337df2f4484d73832211
Kuchto47 <32129035+Kuchto47@users.noreply.github.com>
2017-12-03 13:46:31 +0100

    MileStone 3 initial (#45)
    * repaired equals at DogDTO
    
    * added some implementation to dog facade, service, dto, dao and entity.
    
    * repaired constructors callings
    
    * repaired constructors callings
    
    * improving implementation
    
    * refactoring
    
    * refactoring
    
    * refactoring and customerService method added
    
    * refactoring of testdogfacade
    
    * added missing functionality in ServiceRecord create method in facade + added missing functionality in Employee entity
    
    * changed equals methods for dog and dogdto hasDiscount from '==' to 'equals'
    
    * implemented testCustomerService and prepared testCustomerFacade
    
    * implemented testCustomerService and prepared testCustomerFacade
    
    * customerFacade tests and added some implementation
    
    * updated test
    
    * added author + added test in custoemrservicetest
    
    * finished tests, added equals/hashCode to Address, added @author comments to tests
    
    * added tomcat7 to project and rest module
    
    * initialization of rest api and simple customerscontroller implemented
    
    * added tests

---
+++ dog-barbershop-rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.rest;
+
+/**
+ * @author Martin Kuchar 433499
+ */
+
+public abstract class ApiUris {
+    public static final String ROOT_URI_CUSTOMERS   = "/customers";
+    public static final String ROOT_URI_EMPLOYEES      = "/employees";
+    public static final String ROOT_URI_DOGS     = "/dogs";
+    public static final String ROOT_URI_SERVICES = "/services";
+}
---
+++ dog-barbershop-rest/src/test/java/cz/muni/fi/pa165/rest/controllers/CustomersControllerTest.java
@@ -0,0 +1,143 @@
+package cz.muni.fi.pa165.rest.controllers;
+
+import cz.muni.fi.pa165.RootWebContext;
+import cz.muni.fi.pa165.dto.customer.CustomerDTO;
+import cz.muni.fi.pa165.facade.CustomerFacade;
+import cz.muni.fi.pa165.utils.Address;
+import org.junit.BeforeClass;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * @author Martin Kuchar 433499
+ */
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {RootWebContext.class})
+public class CustomersControllerTest extends AbstractTestNGSpringContextTests {
+    @Mock
+    private CustomerFacade customerFacade;
+
+    @Autowired
+    @InjectMocks
+    private CustomersController customersController;
+
+    private MockMvc mockMvc;
+
+    @BeforeClass
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = standaloneSetup(customersController).setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
+    }
+
+    @Test
+    public void getAllCustomers() throws Exception {
+        doReturn(Collections.unmodifiableList(this.createCustomers())).when(customerFacade).getAllCustomers();
+
+        mockMvc.perform(get("/customers")).andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[?(@.id==1)].surname").value("Mrkvicka"))
+            .andExpect(jsonPath("$.[?(@.id==2)].surname").value("Jozinova"));
+    }
+
+    @Test
+    public void getValidCustomer() throws Exception {
+        List<CustomerDTO> customers = this.createCustomers();
+
+        doReturn(customers.get(0)).when(customerFacade).getCustomerById(1l);
+        doReturn(customers.get(1)).when(customerFacade).getCustomerById(2l);
+
+        mockMvc.perform(get("/customers/1")).andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.surname").value("Mrkvicka"));
+
+        mockMvc.perform(get("/customers/2")).andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.surname").value("Jozinova"));
+
+    }
+
+    @Test
+    public void getValidPhoneNumberCustomers() throws Exception {
+        List<CustomerDTO> customers = this.createCustomers();
+
+        doReturn(Arrays.asList(customers.get(0))).when(customerFacade).getAllCustomersMatchingPhoneNumber("123456789");
+
+        mockMvc.perform(get("/customers/by_number/123456789")).andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[?(@.id==1)].surname").value("Mrkvicka"));
+    }
+
+    @Test
+    public void getValidSurnameCustomers() throws Exception{
+        List<CustomerDTO> customers = this.createCustomers();
+
+        doReturn(Arrays.asList(customers.get(1))).when(customerFacade).getAllCustomersMatchingPhoneNumber("123456789");
+
+        mockMvc.perform(get("/customers/by_surname/Jozinova")).andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[?(@.surname==Jozinova)].id").value(2l));
+    }
+
+    @Test
+    public void getInvalidCustomer() throws Exception {
+        doReturn(null).when(customerFacade).getCustomerById(1l);
+
+        mockMvc.perform(get("/customers/1")).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void getInvalidPhoneNumberCustomers() throws Exception {
+        doReturn(null).when(customerFacade).getAllCustomersMatchingPhoneNumber("192837465");
+
+        mockMvc.perform(get("/customers/by_number/192837465")).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void getInvalidSurnameCustomers() throws Exception {
+        doReturn(null).when(customerFacade).getAllCustomersMatchingSurname("RandomSurname");
+
+        mockMvc.perform(get("/customers/by_surname/RandomSurname")).andExpect(status().is4xxClientError());
+    }
+
+    private List<CustomerDTO> createCustomers() {
+        CustomerDTO c1 = new CustomerDTO();
+        c1.setId(1L);
+        c1.setName("Jozko");
+        c1.setSurname("Mrkvicka");
+        c1.setAddress(new Address("City", "Street", 47));
+        c1.setPhoneNumber("123456789");
+        c1.setServiceRecords(new ArrayList<>());
+
+        CustomerDTO c2 = new CustomerDTO();
+        c2.setId(2L);
+        c2.setName("Mrkva");
+        c2.setSurname("Jozinova");
+        c2.setAddress(new Address("Village", "WithoutStreets", 69));
+        c2.setPhoneNumber("987654321");
+        c2.setServiceRecords(new ArrayList<>());
+
+        return Arrays.asList(c1, c2);
+    }
+}
